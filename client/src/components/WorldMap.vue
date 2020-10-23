963f4744defa3616a11344d316b826314174a0da
CodeClan Student <codeclanstudent@macbook-pro-2.home>
2020-10-23 15:26:58 +0100

    partial Map

---
+++ client/src/components/WorldMap.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="map">
+    <l-map :zoom="zoom" :center="center">
+        <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+    </l-map>
+
+  </div>
+</template>
+
+<script>
+import L from 'leaflet';
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+
+
+export default {
+    name: 'world_map',
+    data (){
+        return{
+            zoom:13,
+            center: L.latLng(47.413220, -1.219482),
+            url:'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+            attribution:'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+            marker: L.latLng(47.413220, -1.219482)
+        },
+    },
+    components: {
+        LMap,
+        LTileLayer,
+        LMarker
+    },
+}
+
+</script>
+
+<style>
+
+#map {
+    height: 95hv;
+}
+
+</style>
