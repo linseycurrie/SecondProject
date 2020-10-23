15864b73032fd16e51a391961ab278a8606fe015
Madeleine Wood <madeleinecwood@gmail.com>
2020-10-23 15:18:33 +0100

    added flex layout to app.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="wrapper">
+      <div id="box-one"> map</div>
+    <div id="box-three"> profile</div>
+    </div>
+    <div id="box-two"> quiz links / images </div>
   </div>
 </template>
 
@@ -18,11 +21,34 @@ export default {
 
 <style>
 #app {
+  display: flex;
+  flex-direction: row wrap;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#wrapper {
+  display: flex;
+  flex-direction: column;
+  width: 70%;
+}
+#box-one{
+  background-color: rgb(210, 241, 209);
+  width: 100%;
+  order: 1;
+}
+#box-two {
+  background-color: cadetblue;
+  order: 2;
+  width: 30%;
+  align-items: stretch;
+}
+#box-three{
+  background-color: rgb(233, 216, 232);
+  width: 100%;
+  order: 3;
 }
 </style>
