1496c841b3aa78edfdc18b9537ffcb8bf6e7a2aa
CodeClan Student <codeclanstudent@CodeClans-MacBook-Pro-2.local>
2020-10-23 15:59:59 +0100

    api added

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,18 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <label for="country_select">Country:</label>
+    <select id="country_select" v-model="selectedCountry">
+      <option disabled value="">Select a country</option>
+      <option v-for="(country, index) in countries" :value="country" :key="index">{{ country.name }}</option>
+    </select>
+
+      <country-detail :selectedCountry="selectedCountry"></country-detail>
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import { eventBus } from '@/main.js';
+import CountryDetail from './components/CountryDetail.vue';
+
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+
+  data() {
+    return {
+      countries: [],
+      selectedCountry: null
+    }
+  },
+
+components: {
+    'country-detail': CountryDetail
+  },
+
+  mounted() {
+    this.fetchCountries()
+  },
+
+  methods: {
+    fetchCountries: function() {
+      const request = fetch("https://restcountries.eu/rest/v2/all")
+      .then(response => response.json())
+      .then(data => this.countries = data)
+      console.log(request)
+    },
   }
+
 }
 </script>
 
---
+++ client/src/components/CountryDetail.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-if="selectedCountry" id="selected-country">
+
+    <h3> Country name:{{ selectedCountry.name }} </h3>
+    <h3> Capital City: {{ selectedCountry.capital }} </h3>
+    <h3> Region: {{ selectedCountry.subregion }}, {{ selectedCountry.region }} </h3>
+
+    <h3> {{ selectedCountry.currencies.name }} </h3>
+  
+  </div>
+
+</template>
+
+<script>
+export default {
+
+    name: 'country-detail',
+    props: ['selectedCountry'],
+
+}
+</script>
+
+<style>
+
+</style>
