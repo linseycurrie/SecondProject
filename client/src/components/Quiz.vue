70fbfb68eb022f544c13531417ffc40c2017dc84
Madeleine Wood <madeleinecwood@gmail.com>
2020-10-24 12:11:21 +0100

    edited quiz

--- client/src/components/Quiz.vue
+++ client/src/components/Quiz.vue
@@ -1,24 +1,38 @@
 <template>
   <div id="quiz">
       This is where the Quiz will go for now
+      <div v-if="(quizList.length <= 4)"> Quiz adequate Length </div>
   </div>
 </template>
 
 <script>
-
+import {eventBus} from '../main.js'
 
 export default {
-name: "quiz",
-props: ['quizList'],
-data() {
-    return {
-        quizList: []
+    name: "quiz",
+    props: ['quizList'],
+    data() {
+        return {
+        
     }
 },
-mounted() {
-    eventBus.$on('addToQuizList', (selectedCountry) => {
-      this.quizList.push(selectedCountry) }) 
-}
+    computed: {
+    
+}, 
+
+    mounted() {
+        eventBus.$on('addToQuizList', (selectedCountry) => {
+        this.quizList.push(selectedCountry) }) ,
+        listLength()
+},   
+    methods()  {
+        listLength = function() {
+        if (this.quizList.length < 4) {
+            this.quizList.splice(0, 1)
+        }
+    }  
+  }
+  
 }
 
 </script>
