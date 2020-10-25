f4cb25a21f0800fc8e0f953d79872805cf634a13
CodeClan Student <codeclanstudent@CodeClans-MacBook-Pro-2.local>
2020-10-25 21:00:04 +0000

    router

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,7 @@
 import 'leaflet/dist/leaflet.css';
-
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
 
 
@@ -11,5 +11,6 @@ Vue.config.productionTip = false
 export const eventBus = new Vue();
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
