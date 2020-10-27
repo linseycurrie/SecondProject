8b83ed7955380cc2c59e133095991114f7af01b6
CodeClan Student <codeclanstudent@CodeClans-MacBook-Pro-2.local>
2020-10-27 16:21:20 +0000

    detail change

--- client/src/components/CountryDetail.vue
+++ client/src/components/CountryDetail.vue
@@ -3,20 +3,11 @@
 
     <h3> {{ selectedCountry.name }} </h3>
     <img class="lrg-flag" :src="selectedCountry.flag" alt="">
-<<<<<<< HEAD
-    <h3> Capital City: {{ selectedCountry.capital }} </h3>
-    <h3> Region: {{ selectedCountry.subregion }}, {{ selectedCountry.region }} </h3>
-    <h3> Area: {{ selectedCountry.area }}km²</h3>
-    <h3> Population: {{ selectedCountry.population }}</h3>
-    <h3> Demonym: {{ selectedCountry.demonym }}</h3>
-
-=======
     <p v-if="selectedCountry.region !==''">{{ selectedCountry.name }} is located in {{ selectedCountry.region }}.</p>
     <p>The size of {{ selectedCountry.name }} is {{ selectedCountry.area.toLocaleString() }}km².</p>
     <p v-if="selectedCountry.capital !==''">The Capital City of {{ selectedCountry.name }} is {{ selectedCountry.capital }}.</p>
     <p>{{ selectedCountry.population.toLocaleString() }} people live in {{ selectedCountry.name }}.</p>
     <p v-if="selectedCountry.demonym !==''">People from {{ selectedCountry.name }} are known as {{ selectedCountry.demonym }}.</p>
->>>>>>> develop
   
   </div>
 
