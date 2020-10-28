794463f1c317a2ac3890dd46b376aa8732f058d1
Madeleine Wood <madeleinecwood@gmail.com>
2020-10-28 11:35:16 +0000

    css fiddling

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,13 +1,19 @@
-<template id="home">
+<template >
+<div>
   <div id="images">
-      <img class="img" src="../assets/Geography.svg" alt="">
-      <img class="img" src="../assets/Maths.svg" alt="">
-      <img class="img" src="../assets/English.svg" alt="">
-      <img class="animate__animated animate__bounce" src="../assets/cute-brain-cartoon.png">
-      <img class="img" src="../assets/Science.svg" alt="">
-      <img class="img" src="../assets/History.svg" alt="">
-      <img class="img" src="../assets/Languages.svg" alt="">
+      <img id="pic" class="img" src="../assets/Geography.svg" alt="">
+      <img id="pic" class="img" src="../assets/Maths.svg" alt="">
+      <img id="pic" class="img" src="../assets/English.svg" alt="">
   </div>
+    <div id="images2">
+      <img id="pic2" class="animate__animated animate__bounce" src="../assets/cute-brain-cartoon.png">
+      </div>
+    <div id="images3">
+      <img id="pic3" class="img" src="../assets/Science.svg" alt="">
+      <img id="pic3" class="img" src="../assets/History.svg" alt="">
+      <img id="pic3" class="img" src="../assets/Languages.svg" alt="">
+  </div>
+    </div>
 </template>
 
 <script>
@@ -18,11 +24,34 @@ export default {
 
 <style lang="css" scoped>
 
+/* #home {
+    display: flex;
+    flex-direction: column
+} */
 
 .img {
 height: 200px;
 width: 200px;
 }
-
-
+ #images {
+     display: flex;
+     flex-direction: column wrap;
+ }
+ #images2 {
+     display: flex;
+     flex-direction: column wrap;
+ }
+  #images3 {
+     display: flex;
+     flex-direction: column wrap;
+ }
+#pic {
+    padding: 10px
+}
+#pic2 {
+    padding: 10px
+}
+#pic3 {
+    padding: 10px
+}
 </style>
