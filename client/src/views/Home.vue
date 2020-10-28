1f828ab5d0a129caf38d1c197cb3bf1cd38e07e7
Madeleine Wood <madeleinecwood@gmail.com>
2020-10-28 10:17:01 +0000

    added quiz markings and some css

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="home">
       
   </div>
 </template>
@@ -11,5 +11,7 @@ export default {
 </script>
 
 <style>
-
+#home {
+  background-image: URL('https://e7.pngegg.com/pngimages/504/365/png-clipart-beige-and-orange-peach-watercolor-background-texture-orange.png')
+}
 </style>
